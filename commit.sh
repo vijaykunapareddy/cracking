ed105e1fed0cea1f5301f630c50867e0aa5da3c9
vijaykunapareddy <kunapareddy.vijay560@gmail.com>
2016-11-04 19:59:46 -0400

    04/11/2016 commit

--- commit.sh
+++ commit.sh
@@ -3,6 +3,6 @@ echo "starting line ..."
 now="$(date +'%d/%m/%Y')"
 git add .; 
 git commit -a -m "$now commit"
-#git commit -am 
+#		git commit -am 
 git push origin master
 echo "code committed ..."
